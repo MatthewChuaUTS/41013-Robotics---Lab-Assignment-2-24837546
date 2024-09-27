928195d401e28decb3c7f0e76418dbcc357882cc
AceOf5pade5 <creativenerf@gmail.com>
2024-09-27 18:43:20 +1000

    Robot stands up now

--- PlateWasherSystem/main.m
+++ PlateWasherSystem/main.m
@@ -1,4 +1,4 @@
 environment();
-currentJointPosition = [-pi/2, 0, -pi/2, 0, -pi/2, 0];  
+currentJointPosition = [0, -pi/2, 0, 0, -pi/2, 0];  
 myUR3 = UR3(transl(1.02, -0.01, 0));                                               % Places the robot at that point
 myUR3.model.animate(currentJointPosition);    
