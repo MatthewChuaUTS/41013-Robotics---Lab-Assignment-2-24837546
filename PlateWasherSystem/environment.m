7181b46e69cd7afc2be3ccf5eb91470fbc364c3a
AceOf5pade5 <creativenerf@gmail.com>
2024-09-27 18:41:15 +1000

    Added UR3 on the table
    yipeeeee

--- PlateWasherSystem/environment.m
+++ PlateWasherSystem/environment.m
@@ -37,7 +37,3 @@ function environment ()
     plotLightCurtain();
  
 end
-
-
-
-
--- PlateWasherSystem/main.m
+++ PlateWasherSystem/main.m
@@ -1,1 +1,4 @@
 environment();
+currentJointPosition = [-pi/2, 0, -pi/2, 0, -pi/2, 0];  
+myUR3 = UR3(transl(1.02, -0.01, 0));                                               % Places the robot at that point
+myUR3.model.animate(currentJointPosition);    
