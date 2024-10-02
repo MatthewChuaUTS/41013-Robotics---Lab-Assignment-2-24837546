949679e3905d22e086efbbf36914ba77493c3cb0
Claudia Wee <Claudia@student.uts.edu.au>
2024-10-02 14:47:06 +1000

    note to Claudia:
    ply files:
    7 ply files - good
    color vertex, face - good
    scale - good
    DH - progress
    blender origin - progress
    testniryo.m

--- PlateWasherSystem/main.m
+++ PlateWasherSystem/main.m
@@ -2,3 +2,4 @@ environment();
 currentJointPosition = [0, -pi/2, 0, 0, -pi/2, 0];  
 myUR3 = UR3(transl(1.02, -0.01, 0));                                               % Places the robot at that point
 myUR3.model.animate(currentJointPosition);    
+% nothing changed - just note
