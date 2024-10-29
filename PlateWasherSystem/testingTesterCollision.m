26f770b13b00f06307999a719f5d2ec399cefee3
AceOf5pade5 <creativenerf@gmail.com>
2024-10-30 10:01:53 +1100

    Working code that we couldn't present :(

--- PlateWasherSystem/testingTesterCollision.m
+++ PlateWasherSystem/testingTesterCollision.m
@@ -2,7 +2,7 @@
 myNiryoOne = niryoOne(transl(0.54, -0.01, 0));
 
 % Define the obstacle (adjust as needed for your environment)
-centerpnt = [0.6, 0.6, 0.3];
+centerpnt = [0,0.4,0.3];
 side = 0.3;
 plotOptions.plotFaces = true;
 [vertex, faces, faceNormals] = RectangularPrism(centerpnt-side/2, centerpnt+side/2, plotOptions);
