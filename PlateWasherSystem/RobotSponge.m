683e3ce1a2e8f5ab23f15f2d4df7f3028bb5a2ac
Claudia Wee <Claudia@student.uts.edu.au>
2024-10-30 01:40:27 +1100

    Robot sponge and plate

---
+++ PlateWasherSystem/RobotSponge.m
@@ -0,0 +1,46 @@
+classdef RobotSponge < handle
+    %ROBOTPIGGY A class that creates a robot piggy
+	%   The piggy can be moved around randomly. It is then possible to query
+    %   the current location (base) of the piggy.    
+    
+    properties
+        %> A single piggy model RobotCows
+        spongeModel;
+    end
+    
+    methods
+        %% Constructor
+        function self = RobotSponge(baseTr)
+            self.spongeModel = self.getSpongeModel('plate');
+            self.spongeModel.base = baseTr;
+            % Plot 3D model
+            % plot3d(self.piggyModel, 0, 'base', false);
+            % self.PlotAndColourRobot();
+            plot3d(self.spongeModel, 0, 'workspace', [-1.6, 0.6, -1.6, 0.6, -0.7971, 1], 'view', [-30,30], 'noarrow', 'nowrist', 'color', {'hotpink'}, 'notiles');
+            hold on;
+        end
+       
+        %% Move the piggy forward and rotate randomly
+        function moveSponge(self, position)
+            % Update piggy position
+            self.spongeModel.base = position;
+            animate(self.spongeModel, 0);                
+            drawnow();
+        end    
+    end
+    
+    methods (Static)
+        %% GetPlateModel
+        function model = getSpongeModel(name)
+            if nargin < 1
+                name = 'Sponge';
+            end
+            
+            [faceData, vertexData] = plyread('sponge.ply', 'tri');
+            link1 = Link('alpha', pi / 2, 'a', 0, 'd', 0.3, 'offset', 0);
+            model = SerialLink(link1, 'name', name);
+            model.faces = {[], faceData};
+            model.points = {[], vertexData};
+        end
+    end    
+end
